4f878131050d5cebefcfd9ab7850f72c7c9f1c80
Bryce Sipes <brycesipes@gmail.com>
2013-03-13 15:51:12 -0400

    Added procedure to project 2.

--- sipes_bryce_assignment2/sipes_bryce_assignment2.js
+++ sipes_bryce_assignment2/sipes_bryce_assignment2.js
@@ -3,13 +3,22 @@
     Bryce Sipes
     SDI 1303
     Project 2
-    Super Hero Battle
+    A Knight's Adventure
 */
 
 // Global Variables
-
+var playerName = "Sir Blondey von Muscles";
+var damageOutput = 15;
+var enemies = ["Frost Troll", "Evil Knight", "Dark Wizard"];
 
 // Procedure
+var readyForAdventure = function(decision){
+    if (decision === "yes"){
+        console.log(playerName + " is ready for an adventure!");
+    } else {
+        console.log(playerName + " is not ready for an adventure");
+    }
+};
 
 
 // Boolean Function
@@ -28,3 +37,4 @@
 
 
 // Main Code
+readyForAdventure(yes);
