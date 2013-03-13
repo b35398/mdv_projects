b6fc043617ab8700a0d264fa0a091c496e1f88fc
Bryce Sipes <brycesipes@gmail.com>
2013-03-13 15:45:32 -0400

    Added global variables to project 2.

--- sipes_bryce_assignment2/sipes_bryce_assignment2.js
+++ sipes_bryce_assignment2/sipes_bryce_assignment2.js
@@ -1,1 +1,30 @@
-alert("JavaScript works!");
+
+/*
+    Bryce Sipes
+    SDI 1303
+    Project 2
+    Super Hero Battle
+*/
+
+// Global Variables
+
+
+// Procedure
+
+
+// Boolean Function
+
+
+// Number Function
+
+
+// String Function
+
+
+// Array Function
+
+
+// Returned Values
+
+
+// Main Code
