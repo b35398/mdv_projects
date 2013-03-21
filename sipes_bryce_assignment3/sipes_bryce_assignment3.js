6125ac6b0f9a4a8c2e825cbdc8c98c0b31a4aef1
Bryce Sipes <brycesipes@gmail.com>
2013-03-20 21:49:44 -0400

    I added a useFlashlight function and a listWeapons function and called them in my main code.

--- sipes_bryce_assignment3/sipes_bryce_assignment3.js
+++ sipes_bryce_assignment3/sipes_bryce_assignment3.js
@@ -12,29 +12,36 @@ var timeOfDay = date.getHours();
 var areaMaP = "abandoned insane asylum";
 var weapons = ["handgun", "shotgun", "crowbar"];
 
+// Functions
+
+var useFlashlight = function(time){
+    if (time >= 19 || time <= 4){
+    playerOne.flashlightOn = true;
+    console.log("I should turn my flashlight on.");
+    } else {
+        console.log("I won't need to use my flashlight at this time of day.");
+    }
+};
+
+var listWeapons = function(weaponArray){
+    var thisWeapon;
+    for (var key in weaponArray){
+        thisWeapon = weaponArray[key]; 
+        console.log("I found a " + thisWeapon + ".");
+    }
+};
+
 // Objects
 
 var playerOne = {
     name        : "Mark",
     weaponArmed : "fists",
-    inured      : false,
-    getWeapon   : function (weapon){  // Method: Procedure
-        weaponArmed = this.weapon;
-    },
-    //flashlight  : function()
-    
+    injuries    : false,
+    getWeapon   : function(weapon){
+        this.weaponArmed = weapon;
+        return this.weaponArmed;
+    }
 };
-
-var group = {
-    sizeOfGroup : 0,
-    groupSize   : function(survivorsLength){   // Method: Mutator
-        this.sizeOfGroup += survivorsLength;
-        return this.sizeOfGroup;
-    },
-    //groupRollCall : function();
-};  // End Group Object
-
-
 
 // JSON Data
 
@@ -61,8 +68,18 @@ var json = {
 
 // Main Code
 
-console.log(timeOfDay);
-var peopleInGroup = group.groupSize(json.survivors.length);;
-console.log(peopleInGroup);
+console.log(playerOne.name + " has found an " + areaMaP + " that looks like it may be a good place to scavenge for supplies.");
+
+useFlashlight(timeOfDay);
+
+console.log(playerOne.name + " quietly makes his way inside the " + areaMaP + " and found a security office that had some weapons.");
+
+listWeapons(weapons);
+
+console.log("Which one should I choose?");
+
+
+
+
 
 
