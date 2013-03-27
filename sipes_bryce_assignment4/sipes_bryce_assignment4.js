3955ffc3347fa4a8c8370e02c772f797707fedb0
Bryce Sipes <brycesipes@gmail.com>
2013-03-27 02:17:39 -0400

    added srraySort function.

--- sipes_bryce_assignment4/sipes_bryce_assignment4.js
+++ sipes_bryce_assignment4/sipes_bryce_assignment4.js
@@ -6,7 +6,7 @@
 // Code Library
 
 var codeLibrary = function(){
-    var isPhoneNum = function(phoneNumber){ // Start of isPhoneNumber function
+    var isPhoneNum = function(phoneNumber){ // Checks if string is a phone number pattern
         var phoneNumberString = phoneNumber;
         var phoneNumberArray = [];
         var isCorrect = true;
@@ -27,18 +27,85 @@ var codeLibrary = function(){
         return isCorrect;
     } // End of isPhoneNumber function
     
-    var isEmail = function(email){ // Start of isEmail function
+    var isEmail = function(email){ // Checks if a string is an email patter 
         var emailStr = email;
         var checkEmail;
         
     } // End of is Email function
     
+    var strToNum = function(stringNum){  // Turns a string number into a number
+        var myStringNum = parseFloat(stringNum);
+        var notANum = "That is not a number";
+        if (!isNaN(myStringNum)){
+            return myStringNum;
+        } else {
+            return notANum;
+        }
+    } // End of strToNum function
+    
+    var isUrl = function(){
+        
+    } // End of isUrl function
+    
+    var totalValue = function(array){ // Finds the total value of numbers in an array
+        var myArray = array;
+        var myTotal = 0;
+        for (var i = 0; i < myArray.length; i++){
+            if (typeof(myArray[i]) === "number"){
+                myTotal += myArray[i];
+            }
+        }
+        return myTotal;
+    } // End of totalValue function
+    
+    var arraySort = function(array, key){
+        var myArray = array.sort(function(a, b) {
+            var x = a[key]; var y = b[key];
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
+        return myArray;
+    } // End of arraySort function
+    
     return {
-        "isPhoneNum": isPhoneNum,
-        "isEmail"   : isEmail,
-      //  "isUrl"     : isUrl
+        "isPhoneNum"  : isPhoneNum,
+        "isEmail"     : isEmail,
+        "isUrl"       : isUrl,
+        "strToNum"    : strToNum,
+        "totalValue"  : totalValue,
+        "arraySort"   : arraySort
+      
     }
 }
 
-var codeLib = codeLibrary();
+// Main Code
+var codeLib = new codeLibrary();
 
+// check isPhoneNum function
+var checkPhoneNumber = codeLib.isPhoneNum("555-555-5555");  // true
+console.log(checkPhoneNumber);
+var checkPhoneNumber = codeLib.isPhoneNum("555-5t5-5555"); // false
+console.log(checkPhoneNumber);
+
+// check isEmail function
+
+// check isUrl function
+
+
+// check stringToNum function
+var stringNumber = codeLib.strToNum("22"); // 22
+console.log(stringNumber);
+var stringNumber = codeLib.strToNum("dog"); // That is not a number.
+console.log(stringNumber);
+
+// check totalValue function
+var crazyArray = [1, "10", false, "cat", 2, 3, "dog", true, 10, "John", false, "11"];
+var totalVal = codeLib.totalValue(crazyArray); // 16
+console.log(totalVal); 
+
+// check arraySort function
+var keyArray = [{b:3},{b:1},{b:5},{b:4},{b:2}];
+var sortedArray = codeLib.arraySort(keyArray,"b");  // b:1, b:2, b:3, b:4, b:5,
+console.log(sortedArray);
+var keyArray = [{a:3},{a:1},{a:5},{a:4},{a:2}];
+var sortedArray = codeLib.arraySort(keyArray,"a");  // a:1, a:2, a:3, a:4, a:5,
+console.log(sortedArray);
